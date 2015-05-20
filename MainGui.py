d2855ca830c9734168cfd455fc8a646482552956
Wiktor Manczarski <cloud99_9@hotmail.com>
2015-05-20 16:05:36 +0200

    Frame und CharacterSets

--- MainGui.py
+++ MainGui.py
@@ -1,7 +1,31 @@
 __author__ = 'Shadow'
 
-import tkinter
+from tkinter import *
 
 
-top = tkinter.Tk()
-top.mainloop()
+class Example(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent, background="white")
+
+        self.parent = parent
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.parent.title("pwgen")
+        self.pack(fill=BOTH, expand=1)
+        txtField = Entry(self,width = 20)
+        txtField.pack(side = TOP)
+
+def main():
+
+    root = Tk()
+    root.geometry("800x600")
+    app = Example(root)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
