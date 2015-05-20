87db1c988a9744a7a149c5ab84f1287ead783c1a
littleLemming <lemming0061@gmail.com>
2015-05-20 17:00:23 +0200

    Patterns

---
+++ Patterns.py
@@ -0,0 +1,11 @@
+__author__ = 'twi'
+
+# a = lower case
+# A = upper case
+# 0 = number
+# # = custom character
+# ".." = string
+# (..) = set of character sets - example: (aA0) - for permutations of lower, upper case and numbers
+pattern = []
+
+def setPattern():
