4cbf1eb2c9a2837b9ff9daf659cf7dffba4c5733
littleLemming <lemming0061@gmail.com>
2015-05-20 16:38:51 +0200

    buildPermutations and setCustom

--- CharacterSets.py
+++ CharacterSets.py
@@ -3,3 +3,10 @@ __author__ = 'Shadow'
 alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 alphaBig = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 num = ['0','1','2','3','4','5','6','7','8','9']
+custom = []
+
+def setCustom(string):
+    custom = []
+    for x in string:
+        custom.append(x)
+    print(custom)
