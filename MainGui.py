8aef882792f285276dda69fac352f2a6264f409b
Wiktor Manczarski <cloud99_9@hotmail.com>
2015-05-21 13:01:45 +0200

    guy update

--- MainGui.py
+++ MainGui.py
@@ -33,33 +33,45 @@ class MainFrame(Frame):
         self.ckbLower = Checkbutton(text='lower case',variable=self.lowerBool).grid(row=1,column=0,padx=3 ,pady=3,sticky='W')
         self.ckbUpper = Checkbutton(text='UPPER CASE',variable=self.upperBool).grid(row=2,column=0,padx=3 ,pady=3,sticky='W')
         self.ckbNumeber = Checkbutton(text='Numbers',variable=self.numberBool).grid(row=3,column=0,padx=3 ,pady=3,sticky='W')
-        self.ckbCustom = Checkbutton(text='Custom',variable=self.customBool,command=checkCustomToggle).grid(row=4,column=0,padx=3 ,pady=3,sticky='W')
+        self.ckbCustom = Checkbutton(text='Custom',variable=self.customBool,command=checkCustomToggle).grid(row=4,column=0,columnspan=2,padx=3 ,pady=3,sticky='W')
 
         self.txtCustom = Entry(width = 30,state='disabled')
-        self.txtCustom.grid(row=4,column=1,padx=3 ,pady=3,sticky='W')
-        self.txtCustom.bind('<Return>', setCustomCharacterSet)
+        self.txtCustom.grid(row=5,column=0,columnspan=2,padx=25 ,pady=3,sticky='WSNE')
+        self.txtCustom.bind('<Return>', setCustomCharacterSet)#
+
+        self.lblLength = Label(text='Password Length:').grid(row=6,column=0,columnspan=2,padx=3 ,pady=3,sticky='')
+        self.spbLength = Spinbox(from_=0, to=20)
+        self.spbLength.grid(row=7,column=0,padx=3 ,pady=3,sticky='WSNE')
 
         #Listbox with the patterns
-
-        self.txtPattern = Entry(width=30)
-        self.txtPattern.grid(row=1,column=4,columnspan=4,padx=3 ,pady=3,sticky='')
 
         self.lblPatterns = Label(text='Patterns: ').grid(row=0,column=4,columnspan=4,padx=3 ,pady=3,sticky='')
 
         self.lsbPatterns = Listbox(height=5,width=25)
-        self.lsbPatterns.grid(row=2,column=4,rowspan=5,columnspan=4,padx=3 ,pady=3, sticky='WSNE')
+        self.lsbPatterns.grid(row=1,column=4,rowspan=5,columnspan=4,padx=3 ,pady=3, sticky='WSNE')
 
-        self.btnAddPattern = Button(text='Add',command=addPattern).grid(row=8,column=4,columnspan=2,padx=3 ,pady=3,sticky='WSNE')
-        self.btnRemovePattern = Button(text='Remove',command=removePattern).grid(row=8,column=6,columnspan=2,padx=3 ,pady=3,sticky='WSNE')
+        self.lblNewPattern = Label(text='New Pattern:').grid(row=6,column=4,padx=3,columnspan=4,pady=3)
+        self.txtPattern = Entry(width=30)
+        self.txtPattern.grid(row=7,column=4,columnspan=4,padx=3 ,pady=3,sticky='WSNE')
+
+        self.lblBegin = Label(text='Begin').grid(row=10,column=4,padx=3 ,pady=3,sticky='WSNE')
+        self.txtBegin = Entry(width=10)
+        self.txtBegin.grid(row=10,column=5,padx=3 ,pady=3,sticky='WSNE')
+        self.lblEnd = Label(text='End').grid(row=10,column=6,padx=3 ,pady=3,sticky='WSNE')
+        self.txtEnd = Entry(width=10)
+        self.txtEnd.grid(row=10,column=7,padx=3 ,pady=3,sticky='WSNE')
+
+        self.btnAddPattern = Button(text='Add',command=addPattern).grid(row=11,column=4,columnspan=2,padx=3 ,pady=3,sticky='WSNE')
+        self.btnRemovePattern = Button(text='Remove',command=removePattern).grid(row=11,column=6,columnspan=2,padx=3 ,pady=3,sticky='WSNE')
 
 
         # Generate Button
         self.v = StringVar()
         self.txtFilePath = Entry(width=30,textvariable=self.v)
-        self.txtFilePath.grid(row=9,column=0,columnspan=2,padx=3 ,pady=3,sticky='WSNE')
+        self.txtFilePath.grid(row=12,column=0,columnspan=7,padx=3 ,pady=3,sticky='WSNE')
         self.btnOpen = Button(text='...',command=openFile)
-        self.btnOpen.grid(row=9,column=3,padx=3 ,pady=3,sticky='WSNE')
-        self.btnGenerate = Button(text='Generate', font=10, command=generate).grid(row=9,column=4,columnspan=4,padx=3 ,pady=3,sticky='WSNE')
+        self.btnOpen.grid(row=12,column=7,padx=3 ,pady=3,sticky='WSNE')
+        self.btnGenerate = Button(text='Generate', font=10, command=generate).grid(row=13,column=0,rowspan=2,padx=3 ,pady=3,sticky='WSNE')
 
 ##################################   End of Class   ########################################
 
