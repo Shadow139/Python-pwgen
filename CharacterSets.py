c027b7d0d69932abc9639750c478d889b3f36bc4
littleLemming <lemming0061@gmail.com>
2015-05-23 20:05:25 +0200

    more things in patterns

--- CharacterSets.py
+++ CharacterSets.py
@@ -6,6 +6,24 @@ num = ['0','1','2','3','4','5','6','7','8','9']
 custom = []
 manditory = []
 
+def noAlpha():
+    alpha = []
+
+def noAlphaBig():
+    alphaBig = []
+
+def noNum():
+    num = []
+
+def alpha():
+    alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+def alphaBig():
+    alphaBig = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+def num():
+    num = ['0','1','2','3','4','5','6','7','8','9']
+
 def setCustom(string):
     custom = []
     for x in string:
