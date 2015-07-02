de74bb9664d64cb4c0c4a5ab2b5378fb12cb3ae0
Shadow139 <cloud99_9@hotmail.com>
2015-07-02 22:27:29 +0200

    permute_wiktor

--- newTryBecause/Permutations.py
+++ newTryBecause/Permutations.py
@@ -1,1 +1,15 @@
 __author__ = 'twi'
+import itertools
+
+
+
+def permute_wiktor(iterable, r):
+    'Alternate version that filters from product()'
+    pool = tuple(iterable)
+    print(pool)
+    n = len(pool)
+    print(n)
+    for indices in itertools.product(range(n), repeat=r):
+        print('--',indices)
+        if sorted(indices) == list(indices):
+            yield tuple(pool[i] for i in indices)
