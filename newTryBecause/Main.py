0a1428f6a408aad9536e8841a8c537b3673c8e3c
Shadow139 <cloud99_9@hotmail.com>
2015-07-02 22:54:10 +0200

    permus

--- newTryBecause/Main.py
+++ newTryBecause/Main.py
@@ -1,1 +1,12 @@
 __author__ = 'twi'
+
+import Permutations
+import itertools
+
+
+permList = list(itertools.product("abcde", repeat = 12))  # This seems ok
+
+
+for x in permList:
+    print(x)
+print(len(permList))
--- newTryBecause/Permutations.py
+++ newTryBecause/Permutations.py
@@ -3,13 +3,24 @@ import itertools
 
 
 
-def permute_wiktor(iterable, r):
+def permute_wiktor(string, length):
     'Alternate version that filters from product()'
-    pool = tuple(iterable)
+    pool = tuple(string)
     print(pool)
     n = len(pool)
     print(n)
-    for indices in itertools.product(range(n), repeat=r):
-        print('--',indices)
+    for indices in itertools.product(range(n), repeat=length):
+        #print('--',indices)
         if sorted(indices) == list(indices):
             yield tuple(pool[i] for i in indices)
+
+
+def product(*args, **kwds):
+    # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
+    # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
+    pools = map(tuple, args) * kwds.get('repeat', 1)
+    result = [[]]
+    for pool in pools:
+        result = [x+[y] for x in result for y in pool]
+    for prod in result:
+        yield tuple(prod)
